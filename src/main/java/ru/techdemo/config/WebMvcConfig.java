79d822a6a3535769ba624376d08a8add71b7fa14
Ushakov Michael <um.nix.user@gmail.com>
2019-10-02 13:10:12 +0300

    pom modification

--- src/main/java/ru/techdemo/config/WebMvcConfig.java
+++ src/main/java/ru/techdemo/config/WebMvcConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;

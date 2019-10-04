05761526d51903e637ee96db3ed8b3e172c48dd3
Ushakov Michael <um.nix.user@gmail.com>
2019-10-04 19:04:32 +0300

    final one, but not clear

--- src/main/java/ru/techdemo/config/Oauth2AuthorizationServerConfigAdapter.java
+++ src/main/java/ru/techdemo/config/Oauth2AuthorizationServerConfigAdapter.java
@@ -33,17 +33,16 @@ public class Oauth2AuthorizationServerConfigAdapter extends AuthorizationServerC
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-            .inMemory()
-            .withClient("clientapp")
-            .secret(passwordEncoder.encode("123456"))
-            .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-            .authorities("READ_ONLY_CLIENT")
-            .scopes("read_profile_info")
-            .resourceIds("oauth2-resource")
-            .redirectUris("http://localhost:8081/login")
-            .accessTokenValiditySeconds(5000)
-            .refreshTokenValiditySeconds(50000);
+        clients.inMemory()
+               .withClient("clientapp")
+               .secret(passwordEncoder.encode("123456"))
+               .authorizedGrantTypes("password", "authorization_code", "refresh_token")
+               .authorities("READ_ONLY_CLIENT")
+               .scopes("read_profile_info")
+               .resourceIds("oauth2-resource")
+               .redirectUris("http://localhost:8081/login")
+               .accessTokenValiditySeconds(5000)
+               .refreshTokenValiditySeconds(50000);
     }
     
     @Autowired
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: michael
-  Date: 02.10.2019
-  Time: 16:47
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>

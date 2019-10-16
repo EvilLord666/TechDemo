264a44652b38da7aee3e7fd164d67d5036ed732a
Ushakov Michael <um.nix.user@gmail.com>
2019-10-16 20:17:45 +0300

    delete non using parts

--- src/main/java/ru/techdemo/security/config/AppSecurityExtServiceConfigAdapter.java
+++ src/main/java/ru/techdemo/security/config/AppSecurityExtServiceConfigAdapter.java
@@ -5,25 +5,10 @@
  */
 package ru.techdemo.security.config;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
-import ru.techdemo.security.client.OpenAmOAuth2Client;
-import ru.techdemo.security.services.RestOAuth2UserService;
 
 
 @Configuration
